161eccef795f8d4defa91d8671818b9791c9b639
MagedKhodair <maged.khodair@gmail.com>
2025-11-11 19:12:41 -0500

    User ID From Firebase 3

--- api.py
+++ api.py
@@ -24,17 +24,16 @@ async def signup_user(user: UserSignUp):
         if existing_users:  # If list is not empty
             raise HTTPException(status_code=400, detail="Email already registered")
         
-        user_id = str(uuid.uuid4())
         created_at = datetime.utcnow()
         
         # Insert new user using execute_command
         await execute_command("""
             INSERT INTO Application_User (user_id, first_name, last_name, address, login_email, created_at)
             VALUES ($1, $2, $3, $4, $5, $6)
-        """, user_id, user.first_name, user.last_name, user.address, user.login_email, created_at)
+        """, user.user_id, user.first_name, user.last_name, user.address, user.login_email, created_at)
         
         return UserResponse(
-            user_id=user_id,
+            user_id=user.user_id,
             first_name=user.first_name,
             last_name=user.last_name,
             address=user.address,
